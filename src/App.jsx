ecd8354eb666ab42ec81f2d9467b4d16c94eaebd
Ankit Singh <ankitjasbeersingh@gmail.com>
2024-10-28 23:45:44 +0530

    add seed pharse

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { generateMnemonic } from 'bip39';
 function App() {
-  const [count, setCount] = useState(0)
+  const [mnenonic, setMnemonic] = useState("");
 
   return (
     <>
+      <button onClick={async function(){
+        const mn = await generateMnemonic();
+        setMnemonic(mn)
+      }}>Create Seed Phrase</button>
       
     </>
   )
